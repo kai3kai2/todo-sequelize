0d22f29573e82433fb429db0c3f73ce6c652a4cc
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-02-26 01:01:33 +0800

    feat: add flash message

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -25,10 +25,28 @@ router.get("/register", (req, res) => {
 
 router.post("/register", (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
+  const errors = [];
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: "所有欄位不能為空" });
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: "密碼與再次輸入的密碼不相符。" });
+  }
+  if (errors.length) {
+    return res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+  }
   User.findOne({ where: { email } }).then((user) => {
     if (user) {
-      console.log("User already exists");
+      errors.push({ message: "這個信箱已經存在!" });
       return res.render("register", {
+        errors,
         name,
         email,
         password,
@@ -52,6 +70,7 @@ router.post("/register", (req, res) => {
 
 router.get("/logout", (req, res) => {
   req.logout();
+  req.flash("success_msg", "成功登出，歡迎再次使用。");
   res.redirect("/users/login");
 });
 
